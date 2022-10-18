4577df99dfa608b719da82976a67cda0b5ff3f00
KPMGE <jesuskevin254@gmail.com>
2022-10-17 21:23:35 -0300

    :sparkles: add example of Rails.env

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,14 @@ module ApplicationHelper
   def br_date(us_date)
     us_date.strftime('%d/%m/%Y')
   end
+
+  def rails_environment
+    if Rails.env.environment?
+      'ENVIRONMENT'
+    elsif Rails.env.production?
+      'PRODUCTION'
+    else
+      'TEST'
+    end
+  end
 end
