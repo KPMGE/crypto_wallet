7bbda41f52364c43988c598c8270a98742f86cbb
KPMGE <jesuskevin254@gmail.com>
2022-11-05 15:23:41 -0300

    :sparkles: add fake data to seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Coin.create!(
+  description: 'Bitcoin',
+  acronym: 'BTC',
+  url_image: 'https://s2.coinmarketcap.com/static/img/coins/200x200/1.png'
+)
+
+Coin.create!(
+  description: 'Etherium',
+  acronym: 'ETC',
+  url_image: 'https://avatars.githubusercontent.com/u/6250754?s=200&v=4'
+)
+
+Coin.create!(
+  description: 'Dash',
+  acronym: 'DSH',
+  url_image: 'https://s2.coinmarketcap.com/static/img/coins/200x200/131.png'
+)
